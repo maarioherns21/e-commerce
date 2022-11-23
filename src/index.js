e014341fc87e3eb5573d295ae63790aa90d1ac93
maarioherns21 <maarioherns21@gmail.com>
2022-11-22 16:11:59 -0800

    new

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+
+import App from "../src/App/App"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  ///the strick mode was delete and now is not rerendering twice fetchin data
+    <App />
+);
